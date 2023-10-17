46f378431d2b6758e2baa29762c44e03d5ab5799
IharDorinel <ihardaronin@gmail.com>
2023-10-17 21:22:33 +0300

    'first'

---
+++ src/router.js
@@ -0,0 +1,24 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+import CoachesList from './pages/coaches/CoachesList';
+import CoachDetails from './pages/coaches/CoachDetails';
+import CoachRegistration from './pages/coaches/CoachRegistration';
+import ContactCoach from './pages/requests/ContactCoach';
+import RequestsReceived from './pages/requests/RequestsReceived';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', redirect: '/coaches'},
+    { path: '/coaches', component: CoachesList },
+    { path: '/coaches/:id', component: CoachDetails,
+    children: [
+      { path: 'contact', component: ContactCoach }
+    ]},
+    { path: '/register', component: CoachRegistration },
+    { path: '/requests', component: RequestsReceived },
+    { path: '/:notFound(.*)', component: null },
+  ]
+})
+
+export default router
