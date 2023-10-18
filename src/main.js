1cc0dbb5eb4c8e67c3e7a0eca01fbe8bc13559b8
IharDorinel <ihardaronin@gmail.com>
2023-10-18 14:09:56 +0300

    create base

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
 import { createApp } from 'vue';
 import router from './router';
+import store from './store/index';
 
 import App from './App.vue';
+import BaseCard from './components/ui/BaseCard';
+import BaseButton from './components/ui/BaseButton';
 
 const app = createApp(App)
 
 app.use(router)
+app.use(store)
+
+app.component('base-card', BaseCard)
+app.component('base-button', BaseButton)
 
 app.mount('#app');
