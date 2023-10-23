6a94c52f1c562e58607dc8d03b77a661c8c50a2a
IharDorinel <ihardaronin@gmail.com>
2023-10-23 15:37:35 +0300

    add forms

--- src/pages/coaches/CoachRegistration.vue
+++ src/pages/coaches/CoachRegistration.vue
@@ -1,4 +1,23 @@
 <template>
-  REGISTER
-
+  <section>
+    <base-card>
+      <h2>Register as a coach now!</h2>
+      <coach-form @save-data="saveData"></coach-form>
+    </base-card>
+  </section>
 </template>
+
+<script>
+import CoachForm from '../../components/coaches/CoachForm';
+import BaseCard from '../../components/ui/BaseCard';
+export default {
+  components: { BaseCard, CoachForm },
+  methods: {
+    saveData(data) {
+      this.$store.dispatch('coaches/registerCoach', data)
+      this.$router.replace('/coaches')
+
+    }
+  }
+}
+</script>
